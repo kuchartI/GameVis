d2acf1c453b41929d0dc0a9ac6f1b4e5caedd77e
ilyaprogger <ilyaprogger>
2019-06-07 15:05:54 +0300

    ;jgf

--- app/src/main/java/com/hfad/viselica/SplashActivity.java
+++ app/src/main/java/com/hfad/viselica/SplashActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
-    private final int SPLASH_DISPLAY = 2000;
+    private final int SPLASH_DISPLAY = 1000;
 
 
     @Override
