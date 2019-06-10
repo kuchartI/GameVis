5383581c2601933f17354f1d129c2ad7e526c67d
ilyaprogger <ilyaprogger>
2019-06-11 00:24:06 +0300

    Хуйня

--- app/src/main/java/com/hfad/viselica/InputWordActivity.java
+++ app/src/main/java/com/hfad/viselica/InputWordActivity.java
@@ -3,6 +3,8 @@ package com.hfad.viselica;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +17,20 @@ public class InputWordActivity extends AppCompatActivity implements View.OnClick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_word);
         et = findViewById(R.id.editText);
+        et.setFilters(new InputFilter[] {
+                new InputFilter() {
+                    public CharSequence filter(CharSequence src, int start,
+                                               int end, Spanned dst, int dstart, int dend) {
+                        if(src.equals("")){
+                            return src;
+                        }
+                        if(src.toString().matches("[а-яА-Я]+")){
+                            return src;
+                        }
+                        return "";
+                    }
+                }
+        });
         Button button = findViewById(R.id.inputText);
         button.setOnClickListener(this);
     }
@@ -22,7 +38,9 @@ public class InputWordActivity extends AppCompatActivity implements View.OnClick
     public void onClick(View v) {
         Intent a = new Intent(this, GameActivity.class);
         a.putExtra("text", et.getText().toString());
+        et.setText("");
         startActivity(a);
+
     }
 }
 
