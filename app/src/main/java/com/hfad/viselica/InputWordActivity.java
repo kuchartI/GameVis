700bbd47c9069c9b15dabd0ab718a1af1753bffb
ilyaprogger <ilyaprogger>
2019-06-13 00:26:26 +0300

    Тут я пытаюсь разобраться с бд!

--- app/src/main/java/com/hfad/viselica/InputWordActivity.java
+++ app/src/main/java/com/hfad/viselica/InputWordActivity.java
@@ -3,8 +3,6 @@ package com.hfad.viselica;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.InputFilter;
-import android.text.Spanned;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
